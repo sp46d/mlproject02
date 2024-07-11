a22b6fca8f4093125fd6a54be6edfa65afb74366
Sanghyuk Park <sanghyuk.park85@gmail.com>
2024-07-11 10:53:40 -0500

    model trainer added

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from textSummarizer.pipeline.stage_02_data_validation import DataValidationPipeline
 from textSummarizer.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
+from textSummarizer.pipeline.stage_04_model_trainer import ModelTrainerPipeline
 from textSummarizer.logging import logger
 
 
@@ -28,8 +29,19 @@ except Exception as e:
 STAGE_NAME = "Data transformation stage"
 try:
     logger.info(f">>> Stage '{STAGE_NAME}' Started <<<")
-    data_validation = DataTransformationTrainingPipeline()
-    data_validation.main()
+    data_transformation = DataTransformationTrainingPipeline()
+    data_transformation.main()
+    logger.info(f">>> Stage '{STAGE_NAME}' Completed <<<\n\nx=========================x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Model training stage"
+try:
+    logger.info(f">>> Stage '{STAGE_NAME}' Started <<<")
+    model_trainer = ModelTrainerPipeline()
+    model_trainer.main()
     logger.info(f">>> Stage '{STAGE_NAME}' Completed <<<\n\nx=========================x")
 except Exception as e:
     logger.exception(e)
